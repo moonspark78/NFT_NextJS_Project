e2910e0c11cd537dbc76fb05ff69b9f38af372fa
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-26 18:57:43 +0100

    Working on the Hero  Component

---
+++ app/Components/Helper/Home/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='w-full flex justify-center flex-col'>
+        <div className='w-4/5 mx-auto items-center grid grid-cols-1 lg:grid-cols-5 gap-8'>
+            {/* Text Content */}
+            <div></div>
+            {/* Image Content */}
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
