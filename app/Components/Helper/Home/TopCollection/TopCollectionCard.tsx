0fcf3155d538e08b8e2d1c9fbb8bc883ce6ea288
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-29 23:27:25 +0100

    working on   TopCollectionCard  Component

--- app/Components/Helper/Home/TopCollection/TopCollectionCard.tsx
+++ app/Components/Helper/Home/TopCollection/TopCollectionCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 
 
@@ -13,7 +14,22 @@ type Props ={
 
 const TopCollectionCard = ({data}: Props) => {
   return (
-    <div>TopCollectionCard</div>
+    <div className='rounded-2xl hover:shadow-md transition-all  duration-150 border-2 cursor-pointer
+     border-gray-300 border-opacity-30 p-5'>
+        <div className='flex items-center space-x-6'>
+            <div>
+                <Image alt="img" src={data.image} width={48} height={48}/>
+            </div>
+            <div>
+                <h1 className='text-base text-blue-950 hover:text-indigo-800 font-bold mb-2'>
+                    {data.name}
+                </h1>
+                <span className='text-sm text-black text-opacity-50'>
+                    {data.eth} ETH
+                </span>
+            </div>
+        </div>
+    </div>
   )
 }
 
