cf023bbce1dcb6616f3dc831f8174de80e183963
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-29 20:46:19 +0100

    working on  TopCollection  component

--- app/Components/Helper/Home/TopCollection/TopCollection.tsx
+++ app/Components/Helper/Home/TopCollection/TopCollection.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const TopCollection = () => {
   return (
-    <div>TopCollection</div>
+    <div className='pt-16 pb-12'>
+      <h1 className='text-center font-bold text-3xl p-2 md:text-4xl text-blue-950'>
+        Top Collection over {" "}
+        <span className='text-amber-600'>Last 24 Hours</span>
+      </h1>
+    </div>
   )
 }
 
