221b0bfd47c373d693920c299c8a009b6e54be79
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-30 18:29:44 +0100

    working on  Category  Component

--- app/Components/Helper/Home/Category/Category.tsx
+++ app/Components/Helper/Home/Category/Category.tsx
@@ -1,8 +1,45 @@
+'use client';
 import React from 'react'
+import Carousel from 'react-multi-carousel';
+import HotBidCard from '../HotBids/HotBidCard';
+import { HotBidData } from '@/data/Data';
+
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1324 },
+    items: 4,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1324, min: 764 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 764, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <Carousel
+      additionalTransfrom={0}
+      arrows={false}
+      autoPlay={true}
+      autoPlaySpeed={5000}
+      centerMode={false}
+      infinite
+      responsive={responsive}
+      itemClass='item'>
+        {HotBidData.map((data) =>{
+          return <div key={data.id}>
+            <HotBidCard data={data}/>
+          </div>
+        })}
+    </Carousel>
   )
 }
 
