c6d6e3f825da1972fe1385395df53698295139d2
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-31 19:09:07 +0100

    working on CreateSellNft  Component

--- app/Components/Helper/Home/CreateSellNft/CreateSellNft.tsx
+++ app/Components/Helper/Home/CreateSellNft/CreateSellNft.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const CreateSellNft = () => {
   return (
-    <div>CreateSellNft</div>
+    <div className='pt-16 pb-20'>
+        <h1 className='text-center font-bold text-3xl p-2 md:text-4xl text-blue-950'>
+            Create and Sell your NFTs
+        </h1>
+    </div>
   )
 }
 
