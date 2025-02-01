016cdb781c2674efda30f13a3729f7c655aa226b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-01 18:48:43 +0100

    working  CreateSellNftCad  component

--- app/Components/Helper/Home/CreateSellNft/CreateSellNftCard.tsx
+++ app/Components/Helper/Home/CreateSellNft/CreateSellNftCard.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image';
 import React from 'react'
 
-const CreateSellNftCard = () => {
+
+type Props= {
+    image: string;
+    title: string;
+}
+
+const CreateSellNftCard = ({image, title}:Props) => {
   return (
-    <div>CreateSellNftCard</div>
+    <div>
+        <Image src={image} alt={title} width={60} height={60} className='mx-auto'/>
+        <h1 className='mt-8 mb-2 text-center text-lg text-blue-950 font-bold'>
+            {title}
+        </h1>
+    </div>
   )
 }
 
