1f473e2bbcf7e2b803d040914193dc78f964ee63
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-03 15:04:41 +0100

    working  Footer  component

--- app/Components/Helper/Home/Footer/Footer.tsx
+++ app/Components/Helper/Home/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -6,6 +7,20 @@ const Footer = () => {
       <div className='w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 pb-8 items-start border-b-[1.5px]
       border-b-slate-300'>
         {/* 1st Footer Part   */}
+        <div>
+          <div className='text-2xl text-red-700 font-semibold mb-4'>
+            NFT Shop
+          </div>
+          <p className='text-gray-700 text-sm mt-2'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis provident aliquid odit.
+          </p>
+          <div className='mt-4 flex items-center space-x-4'>
+            <FaFacebookF/>
+            <FaTwitter/>
+            <FaYoutube/>
+            <FaInstagram/>
+          </div>
+        </div>
       </div>
     </div>
   )
